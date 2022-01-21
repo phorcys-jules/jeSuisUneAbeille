ff4ef42109318b1212a4e6c63bf1eadf2b9ba24c
sakerlucas <sakerlucas@gmail.com>
2022-01-21 14:04:07 +0100

    deplacement plantes

--- src/backOffice/js/plantes.js
+++ src/backOffice/js/plantes.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => 
-    res.status(200).json({
-        Plantes: "Homepage des plantes"
-    })
-);
+// middleware that is specific to this router
+app.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+  });
 
-app.get('/rose', (req, res) => 
-    res.send('Rose')
-);
+// define the home page route
+app.get('/', function(req, res) {
+    res.send('Plante homepage');
+});
+
+app.get('/info', function(req, res) {
+    let qr_code = req.query.qr_code;
+    res.send('Plante homepage');
+});
+
+app.post('/createUpdate', function(req, res) {
+    let nom_latin = req.query.nom_latin;
+    let nom_fr = req.query.nom_fr;
+    let hauteur = req.query.hauteur;
+    let nectar = req.query.nectar;
+    let pollen = req.query.pollen;
+    let miellat = req.query.miellat;
+    let floraison = req.query.floraison;
+    let emplacement = req.query.emplacement;
+    let photo = req.query.photo;
+    let localisation = req.query.localisation;
+    res.status(201).send(`${nom_latin} created`);
+});
+
+app.post('/remove', function(req, res) {
+    let id = req.query.id;
+
+    res.status(2).send(`${id} deleted`);
+});
 
 module.exports = app;
