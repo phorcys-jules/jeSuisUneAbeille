102578b56af2b335a4d9a5975f02ee45e923a5a4
sakerlucas <sakerlucas@gmail.com>
2022-01-21 16:13:58 +0100

    ajout login register

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path');
+const bcrypt = require('bcryptjs')
 const map = require('./src/backOffice/js/map');
 const plantes = require('./src/backOffice/js/plantes');
 const qrcode = require('./src/backOffice/js/qrcode');
@@ -25,6 +26,60 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/src/frontOffice/html/index.html");
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + "/src/frontOffice/html/login.html")
+})
+
+app.post('/login', (req, res) => {
+
+    let pseudo = req.body.pseudo
+    let mdp = req.body.mdp
+
+    sql.query('SELECT * FROM users WHERE pseudo = ?', [pseudo], (err, result) => {
+        if (err) throw err
+
+        if ((result.length) && (bcrypt.compareSync(mdp, result[0].mdp)) && (pseudo == result[0].pseudo)) {
+            res.send('Connecté')
+        } else {
+            res.send('Mauvais pseudo ou mot de passe')
+        }
+    })
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/src/frontOffice/html/register.html')
+})
+
+app.post('/register', (req, res) => {
+    let pseudo = req.body.pseudo
+    let mdp = req.body.mdp
+    let mdpConf = req.body.mdpConf
+
+    sql.query('SELECT pseudo FROM # WHERE pseudo = ?', [pseudo], async (error, results) => {
+
+        if (error) throw error
+
+        if (results.length > 0) {
+            res.send('Pseudo déjà utilisé')
+        }
+        else if (mdp !== mdpConf) {
+            res.send('Mots de passe différents')
+        }
+
+        let hashMdp = await bcrypt.hash(mdp, 8)
+        console.log(hashMdp)
+
+        sql.query('INSERT INTO # SET ?', { pseudo: pseudo, nom: nom, prenom: prenom, mdp: hashMdp }, (error, results) => {
+            if (error) {
+                throw error
+            } else {
+                res.send('Enregistré')
+            }
+        })
+
+    })
+})
 
 app.use('/map', map);
 
