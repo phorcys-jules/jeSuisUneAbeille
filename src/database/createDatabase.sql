59dca3f4f9db8e52421474e3b95dd493eda649f9
Jules <jfrancois968@gmail.com>
2022-01-21 15:27:42 +0100

    🗃️ CRéation database ok

--- src/database/createDatabase.sql
+++ src/database/createDatabase.sql
@@ -1,5 +1,4 @@
-CREATE TABLE plantes
-    {
+CREATE TABLE plantes (
         id int NOT NULL ,
         nom_latin VARCHAR(50),
         nom_fr VARCHAR(50),
@@ -11,31 +10,28 @@ CREATE TABLE plantes
         emplacement VARCHAR(100),
         photo VARCHAR(100),
         localisation VARCHAR(100),
-        PRIMARY KEY (id),
-    };
+        PRIMARY KEY (id)
+    );
 
-CREATE TABLE qr
-    {
+CREATE TABLE qr (
         id int NOT NULL,
         id_plante int,
-        longitude NUMBER(8,6),
-        latitude NUMBER(8,6),
+        longitude NUMERIC(8,6),
+        latitude NUMERIC(8,6),
         PRIMARY KEY (id),
         FOREIGN KEY (id_plante) REFERENCES plantes(id)
-    }
+    );
 
-CREATE TABLE utilisateur
-    {
+CREATE TABLE utilisateur (
         id int NOT NULL,
         mail VARCHAR(500),
         nom VARCHAR(50),
         mdp VARCHAR(254),
         PRIMARY KEY (id)
-    }
+    );
 
-CREATE TABLE ruche
-    {
+CREATE TABLE ruche (
         id int NOT NULL,
         nom VARCHAR(50),
         PRIMARY KEY (id)
-    }
+    );
