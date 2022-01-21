4c4fbee3753e69c3a0a14209e7e6efb70614ea8d
Jules <jfrancois968@gmail.com>
2022-01-21 17:16:49 +0100

    🙈 add db.config to gitignore

--- db.config.js
+++ db.config.js
@@ -1,7 +1,19 @@
+//Pre-prod env : 
+/*
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    PORT:"3308",
-    DB: "beehoneyst"
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "root",
+  PORT:"3308",
+  DB: "beehoneyst"
+};
+*/
+
+//Production Env
+module.exports = {
+    HOST: "sql11.freemysqlhosting.net",
+    USER: "sql11467127",
+    PASSWORD: "LVYUkkhKF5",
+    PORT:"3306",
+    DB: "sql11467127"
   };
