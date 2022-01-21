664babe87bcbd44508b6d01f6d296cab29caa30d
sakerlucas <sakerlucas@gmail.com>
2022-01-21 08:53:47 +0100

    commit

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
-const router = express.Router();
 const path = require('path')
 
 const PORT = process.env.PORT || 8080;
 const app = express()
 
-app.use(express.json());
+const vuesDirectory = path.join(__dirname, './src/frontOffice/html')
+app.use(express.static(vuesDirectory))
 
+const assetsDirectory = path.join(__dirname, './Maquettes')
+app.use(express.static(assetsDirectory))
+
+const stylesDirectory = path.join(__dirname, './src/frontOffice/css')
+app.use(express.static(stylesDirectory))
+
+const scriptsDirectory = path.join(__dirname, './src/frontOffice/js')
+app.use(express.static(scriptsDirectory))
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/src/frontOffice/html/index.html'));
+    res.sendFile("index.html");
+});
+
+app.get('/plantes', function(req, res){
+    res.json({
+        Page : "Plantes"
+    })
+});
+
+app.get('/map', function(req, res){
+    res.json({
+        Page : "Map"
+    })
 });
 
 app.listen(PORT, () => {
